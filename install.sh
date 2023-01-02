d3638a4fae09174e3ea6c4f656580adb6787cd3c
mertyn <22528516+mertyn@users.noreply.github.com>
2023-01-02 20:21:54 +0100

    Added ability to add comments to aliases.txt

--- install.sh
+++ install.sh
@@ -4,8 +4,13 @@ file=aliases.txt
 
 # loop through each line and add alias
 cat $file | while read y; do
+
+    # skip line, if it starts with '#'
+    [[ $y =~ ^#.* ]] && continue
+
     command="git config --global alias.$y"
     eval $command
+
 done
 
 # status message
